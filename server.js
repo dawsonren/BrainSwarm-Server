0e7b7bdb80ba46ac195d2736cc4fde0661486d46
TheEasyLemon <dawsonren@gmail.com>
2023-09-03 12:54:43 -0500

    first commit

---
+++ server.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+const cors = require('cors'); // Import the cors module
+
+const ALLOWED_URL = 'http://localhost:3000'
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server, {
+    cors: {
+        origin: ALLOWED_URL
+    }
+});
+
+// Use cors middleware to allow requests from the React app's domain
+app.use(cors({
+    origin: ALLOWED_URL,
+    methods: ['GET', 'POST'],
+}));
+
+io.on('connection', (socket) => {
+    console.log('A user connected');
+
+    socket.on('join-room', (roomCode) => {
+        socket.join(roomCode); // Join the specified room
+    });
+
+    socket.on('roll', (diceValue) => {
+        const room = Array.from(socket.rooms)[1]; // Get the room code from rooms
+        console.log(`Received roll ${diceValue} in room ${room}`);
+        io.to(room).emit('roll', diceValue); // Emit to everyone in the room
+    });
+
+    socket.on('disconnect', () => {
+        console.log('A user disconnected');
+    });
+});
+
+const PORT = process.env.PORT || 3001;
+server.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
